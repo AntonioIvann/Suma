61cd894d85cc0865d39db04c6941ac38313c7ed4
unknown <rantonioivann@gmail.com>
2024-05-08 21:21:31 -0600

    v3

--- MainSuma.java
+++ MainSuma.java
@@ -1,18 +1,30 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
 
 public class MainSuma {
-    public static void main(String[] args) {
-        Scanner su = new Scanner(System.in);
-        System.out.println("Dame el primer valor: ");
-        int n1 = su.nextInt();
-        System.out.println("Dame el segundo valor: ");
-        int n2 = su.nextInt();
+    public static void main(String[] args) { // Método principal, punto de entrada del programa
+        Scanner su = new Scanner(System.in); // Crea un objeto Scanner llamado su para leer la entrada del usuario
 
-        Suma su1 = new Suma();
+        // Solicita al usuario que ingrese el primer valor
+        System.out.println("Dame el primer valor: ");
+        int n1 = su.nextInt(); // Lee el primer valor proporcionado por el usuario
+
+        // Solicita al usuario que ingrese el segundo valor
+        System.out.println("Dame el segundo valor: ");
+        int n2 = su.nextInt(); // Lee el segundo valor proporcionado por el usuario
+
+        Suma su1 = new Suma(); // Crea un objeto de la clase Suma llamado su1
+
+        // Establece los valores proporcionados por el usuario en el objeto su1
         su1.setN1(n1);
         su1.setN2(n2);
+
+        // Llama al método mostrarResultado() de su1 para mostrar el resultado de la suma
         su1.mostrarResultado();
+
+        // Imprime el resultado de la suma utilizando el método mostrarResultado() de su1
         System.out.println("El resultado de la suma es: " + su1.mostrarResultado());
+
+        // Imprime una representación en cadena del objeto su1 utilizando su método toString()
         System.out.println(su1.toString());
     }
 }
--- Suma.java
+++ Suma.java
@@ -1,37 +1,43 @@
 public class Suma {
     private int n1, n2, res;
 
+    // Método para realizar la suma de los dos operandos y devolver el resultado
     public int mostrarResultado() {
-        res = n1 + n2;
-        return res;
+        res = n1 + n2; // Suma los dos operandos y guarda el resultado en la variable res
+        return res; // Devuelve el resultado de la suma
     }
 
+    // Métodos getter y setter para el primer operando (n1)
     public int getN1() {
-        return n1;
+        return n1; // Devuelve el valor del primer operando
     }
 
     public void setN1(int n1) {
-        this.n1 = n1;
+        this.n1 = n1; // Establece el valor del primer operando con el valor proporcionado
     }
 
+    // Métodos getter y setter para el segundo operando (n2)
     public int getN2() {
-        return n2;
+        return n2; // Devuelve el valor del segundo operando
     }
 
     public void setN2(int n2) {
-        this.n2 = n2;
+        this.n2 = n2; // Establece el valor del segundo operando con el valor proporcionado
     }
 
+    // Métodos getter y setter para el resultado de la suma
     public int getRes() {
-        return res;
+        return res; // Devuelve el resultado de la suma
     }
 
     public void setRes(int res) {
-        this.res = res;
+        this.res = res; // Establece el resultado de la suma con el valor proporcionado
     }
 
+    // Método toString para representar el objeto Suma como una cadena de texto
     @Override
     public String toString() {
+        // Retorna una cadena que representa el objeto Suma con sus valores actuales
         return "Suma [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]";
     }
 }
